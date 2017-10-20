8e7ff726eafd21db7308cad75007a698568fd240
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-10-20 10:10:57 +0200

    Add offer accept/decline

--- src/ui/cars/components/car-item.js
+++ src/ui/cars/components/car-item.js
@@ -1,31 +1,53 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { Component } from 'react';
+import API from '../../../api';
 import './car-item.css';
 
-export const CarItem = (car) => {
-  return (
-    <div className="box car">
-      <article className="media">
-        <div className="media-left">
-          <figure className="image is-64x64">
-            <img src={car.image || 'https://bulma.io/images/placeholders/128x128.png'} alt="" />
-          </figure>
-        </div>
-        <Link to={`/cars/${car.numberplate}`} className="car-detail">
-          <div className="media-content">
-            <div className="content">
-              <strong>{car.brand} {car.model}</strong>
-              <br />
-              <p>{car.km} km</p>
-              {car.pending &&
-                <p>Your offer is pending acceptation</p>}
-              {car.propositionBuyer &&
-                <p>This car received an offer !</p>
-              }
-            </div>
+export class CarItem extends Component {
+  accept(isAccepted) {
+    API.post('com.epsi.blockchain.HandleProposition', {
+      car: `${this.props.numberplate}`,
+      status: isAccepted ? 'OK' : 'KO',
+      password: this.user.password,
+    })
+  }
+
+  render() {
+    this.user = JSON.parse(localStorage.getItem('auth'));
+    return (
+      <div className="box car">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.props.image || 'https://bulma.io/images/placeholders/128x128.png'} alt="" />
+            </figure>
           </div>
-        </Link>
-      </article>
-    </div>
-  );
+          <Link to={`/cars/${this.props.numberplate}`} className="car-detail">
+            <div className="media-content">
+              <div className="content">
+                <strong>{this.props.brand} {this.props.model}</strong>
+                <br />
+                <p>{this.props.km} km</p>
+                {this.props.pending &&
+                  <p>Your offer is pending acceptation</p>}
+                {this.props.propositionBuyer &&
+                  <p>This car received an offer !</p>
+                }
+              </div>
+            </div>
+          </Link>
+          <div className="actions">
+            {this.props.owner && this.props.owner === `resource:com.epsi.blockchain.Person#${this.user.email}` && this.props.status === 'PENDING' &&
+              <button onClick={() => this.accept(true)} className="button is-primary accept-offer">
+                <i className="fa fa-check" />
+              </button>}
+            {this.props.owner && this.props.owner === `resource:com.epsi.blockchain.Person#${this.user.email}` && this.props.status === 'PENDING' &&
+              <button onClick={() => this.accept(false)} className="button is-secondary">
+                <i className="fa fa-close" />
+              </button>}
+          </div>
+        </article>
+      </div>
+    );
+  }
 };
